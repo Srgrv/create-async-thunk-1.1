40064d024079634219936d023b491cb79458373e
Sergey Georgiev <sgrgv_art@mail.ru>
2023-06-01 20:14:21 +0300

    create index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 // import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+
+//store
+import store from "./store/store";
+
+//components
+import App from "./components/App";
 
 const root = createRoot(document.getElementById("root"));
-root.render("Hlel");
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
